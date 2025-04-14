f6acb741aada2de4411d90097444178045fc477b
catalinaaprilrodriguez <rodriguezc6@ufl.edu>
2025-04-14 16:07:44 -0400

    -m "modified calculator p1"

--- calculator.py
+++ calculator.py
@@ -32,3 +32,28 @@ def logarithm(a, b):
 def exponent(a, b):
     value = a ** b
     return value
+
+#partner 2 part
+
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero")
+    return a / b
+
+def log(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError("Invalid input for logarithm")
+    return math.log(b, a)
+
+def exp(a, b):
+    return a ** b
